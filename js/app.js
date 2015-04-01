58a8d74f581e7cfd706f5087112ae53d410c68b6
Jesus Menera <j.menera209@gmail.com>
2015-03-31 21:23:22 -0700

    player moves and resets when collides, work on ai.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,129 @@
+/*
+Global Game Settings
+*/
+var GameSetting = function() {
+    this.enemyXDiff = 100;
+    this.score = 0;
+}
+
+var GameAI = function(enemyArray) {
+    if (enemyArray == 'undefined') {
+        enemyArray = [];
+    }
+    this.nextRowCalled = 0;
+    this.activeEnemyArray = enemyArray;
+    this.inactiveEnemyArray = [];
+    this.numberOfAllowedEnemies = 3;
+    this.numberOfCreatedEnemies = 0;
+}
+
+GameAI.prototype.getNextRow = function() {
+    var temp = this.nextRowCalled;
+    this.nextRowCalled++;
+    if(this.nextRowCalled > 2) {
+        this.nextRowCalled = 0;
+    }
+    return temp;
+}
+GameAI.prototype.update = function(dt) {
+    if(this.activeEnemyArray.length === 0){
+       this.activeEnemyArray.push(this.newEnemyCallback());
+    }
+}
+GameAI.prototype.addNewEnemyCallback = function(row) {
+    if (this.inactiveEnemyArray.length > 0) {
+        console.log("insert from inactiveEnemyArray");
+        this.activeEnemyArray.push(this.inactiveEnemyArray.pop());
+        var enemyInSameRow;
+        console.log(row);
+    }
+    else if ( this.numberOfCreatedEnemies < this.numberOfAllowedEnemies - 1) {
+        console.log("new Enemy created");
+        this.activeEnemyArray.push(this.newEnemyCallback());
+        this.numberOfCreatedEnemies++;
+    }
+}
+GameAI.prototype.resetEnemyCallback = function(enemy){
+    var indexOfEnemy = this.activeEnemyArray.indexOf(enemy);
+    this.activeEnemyArray.splice(indexOfEnemy,1);
+    this.inactiveEnemyArray.push(enemy);
+}
+
+var CollisionHandler = function(obj, paramXOffSet, paramYOffSet, paramWidth, paramHeight) {
+    this.xOffSet = paramXOffSet;
+    this.yOffSet = paramYOffSet;
+    this.width = paramWidth;
+    this.height = paramHeight;
+    this.obj = obj;
+}
+
+CollisionHandler.prototype.x = function() {
+    return this.obj.x + this.xOffSet;
+}
+CollisionHandler.prototype.y = function() {
+    return this.obj.y + this.yOffSet;
+}
+
+function haveCollided(entity0, entity1) {
+    if (entity0!= undefined && entity1 != undefined ) {
+        var x0 = entity0.x();
+        var y0 = entity0.y();
+        var x1 = entity1.x();
+        var y1 = entity1.y();
+
+        var deltaY;
+        if (entity0.y > entity1.y) {
+            deltaY = y0 - y1 - entity1.height;
+        }
+        else {
+            deltaY = y1 - y0 - entity0.height;
+        }
+        if (deltaY <= 0) {
+            var deltaX;
+            if (x0 > x1) {
+                deltaX = x0 - x1 - entity1.width;
+            }
+            else {
+                deltaX = x1 - x0 - entity0.width;
+            }
+            if (deltaX <= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return undefined;
+}
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(row=0) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.stepCount = 0;
+    this.stepDeltaX = 100;
+    this.stepSpeed = 100;
+    this.row = -1;
 
+    this.xLeftLimit = -98; //left and right most x limits on screen
+    this.xRightLimit = 505;
+    this.x = this.xLeftLimit;
+    switch(row){
+        case 0:
+            this.y = 60;
+            this.row = 0;
+            break;
+        case 1:
+            this.y = 145;
+            this.row = 1;
+            break;
+        case 2:
+            this.y = 225;
+            this.row = 2;
+            break;
+        default:
+            this.y = 60;
+            this.row = 0;
+    }
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,22 +135,163 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x >= this.xRightLimit) {
+        this.x = this.xLeftLimit;
+    }
+    else{
+        this.x = this.x + (this.stepSpeed * dt); //possible difficult var inplace of 100;
+    }
+
+    this.stepCount++;
+    if(this.stepCount >= this.stepDeltaX) {
+        this.stepCount=0;
+        this.stepCountChangedCallback(this);
+    }
+    if (this.x >= this.xRightLimit) {
+        console.log('x reached');
+        this.xRightLimitReachedCallback(this);
+    }
 }
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    if (this.x >= this.xLeftLimit || this.x <= this.xRightLimit) {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        //ctx.rect(this.collisionHandler.x(), this.collisionHandler.y(), this.collisionHandler.width,this.collisionHandler.height);
+        //ctx.stroke();
+    }
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 320;
+    this.stepPowerUp = 0;
+}
 
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if(this.y == -5) {
+        this.reachedGoalCallback();
+    }
+}
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    /*ctx.beginPath();
+    ctx.arc(this.x+50, this.y+103,36, 0, 2 * Math.PI, false);
+    ctx.stroke();
+    ctx.closePath();
+    */
+    //ctx.rect(this.collisionHandler.x(), this.collisionHandler.y(), this.collisionHandler.width,this.collisionHandler.height);
+    //ctx.stroke();
 
+}
+Player.prototype.reset = function(){
+    this.x = 200;
+    this.y = 320;
+}
+Player.prototype.handleInput = function(movement){
+    var TAKE_WHOLE_STEP = 30 + (10*this.stepPowerUp); //60 limit
+    var TAKE_TINY_STEP = 5;
+    switch(movement){
+        case 'left':
+            if( this.x - TAKE_WHOLE_STEP > -10){
+                this.x -= TAKE_WHOLE_STEP;
+            }
+            else if( this.x - TAKE_TINY_STEP > -10){
+                this.x -= TAKE_TINY_STEP;
+            }
+            break;
+        case 'up':
+            if( this.y - TAKE_WHOLE_STEP > -10){
+                this.y -= TAKE_WHOLE_STEP;
+            }
+            else if( this.y - TAKE_TINY_STEP > -10){
+                this.y -= TAKE_TINY_STEP;
+            }
+            break;
+        case 'right':
+            if( this.x + TAKE_WHOLE_STEP < 412){
+                this.x += TAKE_WHOLE_STEP;
+            }
+            else if( this.x + TAKE_TINY_STEP < 412){
+                this.x += TAKE_TINY_STEP;
+            }
+            break;
+        case 'down':
+            if( this.y + TAKE_WHOLE_STEP < 430){
+                this.y += TAKE_WHOLE_STEP;
+            }
+            else if( this.y + TAKE_TINY_STEP < 430){
+                this.y += TAKE_TINY_STEP;
+            }
+            break;
+    }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player();
+player.collisionHandler =  new CollisionHandler(player,30,113,44,27);
+player.reachedGoalCallback = function() {
+    gameSetting.addToScore(10);
+    var tempGameScore = gameSetting.getScore();
+
+    if(tempGameScore >= 40 && tempGameScore <80) {
+        this.stepPowerUp = 1;
+        gameAI.numberOfAllowedEnemies = 4;
+    }
+    else if (tempGameScore >= 80 && tempGameScore <120) {
+        this.stepPowerUp = 2;
+        gameAI.numberOfAllowedEnemies = 5;
+    }
+    else if (tempGameScore >= 120) {
+        this.stepPowerUp = 3;
+        gameAI.numberOfAllowedEnemies = 6;
+    }
+
+    this.reset();
+}
+
+var allEnemies = [];
+
+var gameSetting = new GameSetting();
+gameSetting.addToScore = function(inc) {
+    this.score += inc;
+}
+gameSetting.getScore = function() {
+    return this.score;
+}
+
+var gameAI = new GameAI(allEnemies);
+gameAI.newEnemyCallback = function() {
+    var newEnemy = new Enemy(this.getNextRow());
+    newEnemy.collisionHandler = new CollisionHandler(newEnemy,3,103,95,25);
+    newEnemy.stepCountChangedCallback = function(me) {
+        if (me.x > 150) {
+            gameAI.addNewEnemyCallback(me.row);
+        }
+    }
+    newEnemy.xRightLimitReachedCallback = function(me) {
+        gameAI.resetEnemyCallback(me);
+    }
+    return newEnemy;
+};
+
+/*
+for(var numberOfEnemies = 0; numberOfEnemies < gameSetting.numberOfEnemies ; numberOfEnemies++) {
+    var newEnemy = new Enemy(numberOfEnemies%3);
+    newEnemy.collisionHandler = new CollisionHandler(newEnemy,3,103,95,25);
+    allEnemies.push(newEnemy);
+}
+*/
 
 
 // This listens for key presses and sends the keys to your
@@ -42,5 +304,8 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    if(e.keyCode >=37 || e.keyCode <=40) {
+        player.handleInput(allowedKeys[e.keyCode]);
+    }
+
 });
--- js/engine.js
+++ js/engine.js
@@ -79,6 +79,7 @@ var Engine = (function(global) {
      * on the entities themselves within your app.js file).
      */
     function update(dt) {
+        gameAI.update(dt);
         updateEntities(dt);
         // checkCollisions();
     }
@@ -94,6 +95,7 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
+
         player.update();
     }
 
@@ -148,6 +150,7 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
