f2f68c4f2f215d3d321290c5b2a3ffc31cc10691
Jesus Menera <j.menera209@gmail.com>
2015-04-22 06:17:48 -0700

    added backbone and dependencies

--- js/gameInterface.js
+++ js/gameInterface.js
@@ -1,3 +1,10 @@
-$().ready(function(){
-	$('canvas').appendTo($("#game-screen"));
+$(function(){
+	var GameScreenView = Backbone.View.extend({
+		initialize: function() {
+			$('body').prepend("<div id='game-screen'></div>")
+			$('canvas').appendTo($("#game-screen"));
+		}
+	});
+
+	var GameScreen = new GameScreenView();
 });
