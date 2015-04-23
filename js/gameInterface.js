4f7607a862160ef9c02677e67ec87f3bb917284b
Jesus Menera <j.menera209@gmail.com>
2015-04-23 06:43:18 -0700

    cleaned up gameInterface, added router. Does not seem to work. Fix sync problem

--- js/gameInterface.js
+++ js/gameInterface.js
@@ -12,34 +12,38 @@ $(function(){
         	this.infoCanvasContext = this.canvas.getContext('2d');
         	this.infoCanvasContext.font = '20pt Calibri';
         	$(this.canvas).appendTo($('#player-info'));
-        	//this.infoCanvas.font="30px Vernada";
-        	//this.infoCanvas.fillText("hello!",20,40);
 		},
 		render: function(options) {
 			if(options === undefined) {
 				options = {heartNum:3,score:0};
 			}
-
 			this.infoCanvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 			var heart_xOrigin = 430;
 			var heart_yOrigin = 40;
 			var heart_xScale = 40;
 			var heart_yScale = 60;
 			var heart_dx = 42;
-
 			for(var x=0; x < options.heartNum ; x++) {
 				var heart_pos = heart_xOrigin - (heart_dx * x);
 				this.infoCanvasContext.drawImage(window.Resources.get('images/Heart.png'),heart_pos,heart_yOrigin,heart_xScale ,heart_yScale);
 			}
-
       		this.infoCanvasContext.fillText('Score:  ' + options.score , 30, 85);
-			//this.infoCanvas.font="30px Vernada";
-        	//this.infoCanvas.fillText("hello!",20,40);
-        	//console.log(window.Resources.get(""))
         	return this;
 		}
 	});
 
-	var GameScreen = new GameScreenView();
-	GameScreen.render();
+	var GameActionsRouter = Backbone.Router.extend({
+		routes: {
+			"/": "start"
+		},
+		start: function() {
+			alert("start");
+		}
+	});
+
+
+	//var GameScreen = new GameScreenView();
+	//var GameAction = new GameActionsRouter();
+
+	//Backbone.history.start({pushState: true})
 });
