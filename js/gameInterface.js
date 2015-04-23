470f1b2d2fe5a2cc437a57fb9ace26c261938032
Jesus Menera <j.menera209@gmail.com>
2015-04-22 20:36:31 -0700

    info canvas renders score. Clears canvas before rendering.

--- js/gameInterface.js
+++ js/gameInterface.js
@@ -10,11 +10,16 @@ $(function(){
     		this.canvas.height = 110;
 
         	this.infoCanvasContext = this.canvas.getContext('2d');
+        	this.infoCanvasContext.font = '20pt Calibri';
         	$(this.canvas).appendTo($('#player-info'));
         	//this.infoCanvas.font="30px Vernada";
         	//this.infoCanvas.fillText("hello!",20,40);
 		},
-		render: function() {
+		render: function(options) {
+			if(options === undefined) {
+				options = {heartNum:3,score:0};
+			}
+
 			this.infoCanvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 			var heart_xOrigin = 430;
 			var heart_yOrigin = 40;
@@ -22,10 +27,12 @@ $(function(){
 			var heart_yScale = 60;
 			var heart_dx = 42;
 
-			for(var x=0; x < 3; x++) {
+			for(var x=0; x < options.heartNum ; x++) {
 				var heart_pos = heart_xOrigin - (heart_dx * x);
 				this.infoCanvasContext.drawImage(window.Resources.get('images/Heart.png'),heart_pos,heart_yOrigin,heart_xScale ,heart_yScale);
 			}
+
+      		this.infoCanvasContext.fillText('Score:  ' + options.score , 30, 85);
 			//this.infoCanvas.font="30px Vernada";
         	//this.infoCanvas.fillText("hello!",20,40);
         	//console.log(window.Resources.get(""))
