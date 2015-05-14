baec0720e05cccbc6b22d4c4c38ba771ce051619
Jesus Menera <j.menera209@gmail.com>
2015-05-14 06:19:00 -0700

    changed tabs to spaces format in gameInterface.

--- js/gameInterface.js
+++ js/gameInterface.js
@@ -1,70 +1,76 @@
-$(function(){
-	var GameScreenView = Backbone.View.extend({
-		initialize: function() {
-			$('#game-container').prepend('<div id="game-screen" class="row game-screen"></div>');
-			$('canvas').appendTo($("#game-screen"));
-			$('#game-screen').append('<div id="player-info" class="player-info"></div>');
-			this.el = '#game-screen';
 
-			/*Set up canvas to display  Score and player lives*/
-			this.canvas =  document.createElement('canvas');
-			this.canvas.width = 505;
-    		this.canvas.height = 606;
+$(function() {
+    var GameScreenView = Backbone.View.extend({
+        initialize: function() {
+            $('#game-container').prepend('<div id="game-screen" class="row game-screen"></div>');
+            $('canvas').appendTo($("#game-screen"));
+            $('#game-screen').append('<div id="player-info" class="player-info"></div>');
+            this.el = '#game-screen';
 
-        	this.infoCanvasContext = this.canvas.getContext('2d');
-        	this.infoCanvasContext.font = '20pt Calibri';
-        	$(this.canvas).appendTo($('#player-info'));
-		},
-		render: function(options) {
-			/*Update game info display if changes occur. */
-			if(options === undefined) {
-				options = {lives:3,score:0};
-			}
-			this.infoCanvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
-			var heart_xOrigin = 430;
-			var heart_yOrigin = 40;
-			var heart_xScale = 40;
-			var heart_yScale = 60;
-			var heart_dx = 42;
-			for(var x=0; x < options.lives ; x++) {
-				var heart_pos = heart_xOrigin - (heart_dx * x);
-				this.infoCanvasContext.drawImage(window.Resources.get('images/Heart.png'),heart_pos,heart_yOrigin,heart_xScale ,heart_yScale);
-			}
-      		this.infoCanvasContext.fillText('Score:  ' + options.score , 30, 85);
-        	return this;
-		},
-		hide: function() {
-			$("#player-info").hide();
-		}
-	});
+            /*Set up canvas to display  Score and player lives*/
+            this.canvas = document.createElement('canvas');
+            this.canvas.width = 505;
+            this.canvas.height = 606;
 
-	var GameActionsRouter = Backbone.Router.extend({
-		routes: {
-			'game': 'game',
-			'leaderboard':'leaderboard',
-			'about':'about'
-		}
- 	});
+            this.infoCanvasContext = this.canvas.getContext('2d');
+            this.infoCanvasContext.font = '20pt Calibri';
+            $(this.canvas).appendTo($('#player-info'));
+        },
+        render: function(options) {
+            /*Update game info display if changes occur. */
+            if (options === undefined) {
+                options = {
+                    lives: 3,
+                    score: 0
+                };
+            }
+            this.infoCanvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            var heart_xOrigin = 430;
+            var heart_yOrigin = 40;
+            var heart_xScale = 40;
+            var heart_yScale = 60;
+            var heart_dx = 42;
+            for (var x = 0; x < options.lives; x++) {
+                var heart_pos = heart_xOrigin - (heart_dx * x);
+                this.infoCanvasContext.drawImage(window.Resources.get('images/Heart.png'), heart_pos, heart_yOrigin, heart_xScale, heart_yScale);
+            }
+            this.infoCanvasContext.fillText('Score:  ' + options.score, 30, 85);
+            return this;
+        },
+        hide: function() {
+            $("#player-info").hide();
+        }
+    });
 
-	var GameScreen = new GameScreenView();
+    var GameActionsRouter = Backbone.Router.extend({
+        routes: {
+            'game': 'game',
+            'leaderboard': 'leaderboard',
+            'about': 'about'
+        }
+    });
 
-	/*Global var to handle game events.*/
-	var dispatcher = _.clone(Backbone.Events);
+    var GameScreen = new GameScreenView();
 
-	/*Call if player score or lives changes.*/
-	dispatcher.on("player-info-render",function(options){
-		GameScreen.render(options);
-	});
-	dispatcher.on("player-info-hide",function(){
-		GameScreen.hide();
-	});
+    /*Global var to handle game events.*/
+    var dispatcher = _.clone(Backbone.Events);
 
-	window.GameScreenDispatcher = dispatcher;
+    /*Call if player score or lives changes.*/
+    dispatcher.on("player-info-render", function(options) {
+        GameScreen.render(options);
+    });
+    dispatcher.on("player-info-hide", function() {
+        GameScreen.hide();
+    });
 
-	var GameActions = new GameActionsRouter;
-	$("#game-new").on('click',function() {
-		GameActions.navigate("#");
-		GameActions.navigate("newgame", {trigger: true});
-	});
-	Backbone.history.start();
+    window.GameScreenDispatcher = dispatcher;
+
+    var GameActions = new GameActionsRouter;
+    $("#game-new").on('click', function() {
+        GameActions.navigate("#");
+        GameActions.navigate("newgame", {
+            trigger: true
+        });
+    });
+    Backbone.history.start();
 });
